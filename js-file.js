cffdae43f2b9503f31475d27863cc99fc5f09268
Becky Nasty <99774559+aurelius250@users.noreply.github.com>
2022-12-29 11:23:30 -0800

    Add JS dom manipulation exercise

---
+++ js-file.js
@@ -0,0 +1,29 @@
+const redParagraph = document.createElement("p"); 
+redParagraph.classList.add("red-paragraph")
+redParagraph.style.color = "red"; 
+redParagraph.textContent = "Hey I'm red!";
+const body = document.querySelector("body"); 
+body.appendChild(redParagraph);
+
+const blueHeading = document.createElement("h3");
+blueHeading.classList.add("blue-heading"); 
+blueHeading.style.color = "blue";
+blueHeading.textContent = "I'm a blue h3!"
+body.appendChild(blueHeading);
+
+const pinkDiv = document.createElement("div"); 
+pinkDiv.classList.add("pink-div"); 
+pinkDiv.style.backgroundColor = "pink"; 
+pinkDiv.style.border = "solid black 2px"; 
+
+const headingInDiv = document.createElement("h1"); 
+headingInDiv.classList.add("heading-in-div"); 
+headingInDiv.textContent = "I'm in a div"; 
+pinkDiv.append(headingInDiv);
+
+const paragraphInDiv = document.createElement("p"); 
+paragraphInDiv.textContent = "ME TOO!"; 
+paragraphInDiv.classList.add("paragraph-in-div");
+pinkDiv.append(paragraphInDiv);
+
+body.appendChild(pinkDiv);
