90f8e062f6c2ef89bdb0b8e6703c669797ca6cec
thomaschor <99774559+thomaschor@users.noreply.github.com>
2023-01-30 19:33:49 -0800

    Advanced selectors files and table practice add

--- js-file.js
+++ js-file.js
@@ -1,29 +1,43 @@
-const redParagraph = document.createElement("p"); 
-redParagraph.classList.add("red-paragraph")
-redParagraph.style.color = "red"; 
-redParagraph.textContent = "Hey I'm red!";
-const body = document.querySelector("body"); 
-body.appendChild(redParagraph);
+// const redParagraph = document.createElement("p"); 
+// redParagraph.classList.add("red-paragraph")
+// redParagraph.style.color = "red"; 
+// redParagraph.textContent = "Hey I'm red!";
+// const body = document.querySelector("body"); 
+// body.appendChild(redParagraph);
 
-const blueHeading = document.createElement("h3");
-blueHeading.classList.add("blue-heading"); 
-blueHeading.style.color = "blue";
-blueHeading.textContent = "I'm a blue h3!"
-body.appendChild(blueHeading);
+// const blueHeading = document.createElement("h3");
+// blueHeading.classList.add("blue-heading"); 
+// blueHeading.style.color = "blue";
+// blueHeading.textContent = "I'm a blue h3!"
+// body.appendChild(blueHeading);
 
-const pinkDiv = document.createElement("div"); 
-pinkDiv.classList.add("pink-div"); 
-pinkDiv.style.backgroundColor = "pink"; 
-pinkDiv.style.border = "solid black 2px"; 
+// const pinkDiv = document.createElement("div"); 
+// pinkDiv.classList.add("pink-div"); 
+// pinkDiv.style.backgroundColor = "pink"; 
+// pinkDiv.style.border = "solid black 2px"; 
 
-const headingInDiv = document.createElement("h1"); 
-headingInDiv.classList.add("heading-in-div"); 
-headingInDiv.textContent = "I'm in a div"; 
-pinkDiv.append(headingInDiv);
+// const headingInDiv = document.createElement("h1"); 
+// headingInDiv.classList.add("heading-in-div"); 
+// headingInDiv.textContent = "I'm in a div"; 
+// pinkDiv.append(headingInDiv);
 
-const paragraphInDiv = document.createElement("p"); 
-paragraphInDiv.textContent = "ME TOO!"; 
-paragraphInDiv.classList.add("paragraph-in-div");
-pinkDiv.append(paragraphInDiv);
+// const paragraphInDiv = document.createElement("p"); 
+// paragraphInDiv.textContent = "ME TOO!"; 
+// paragraphInDiv.classList.add("paragraph-in-div");
+// pinkDiv.append(paragraphInDiv);
 
-body.appendChild(pinkDiv);
+// body.appendChild(pinkDiv);
+
+// const btn = document.querySelector('#btn');
+
+// btn.addEventListener("click", function (e) { 
+//     e.target.style.background = "blue";
+// });
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => { 
+    button.addEventListener("click", () => { 
+        alert(button.id); 
+    })
+})
